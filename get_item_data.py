2f650a999f05cba00ba612564d33a0bbcc70f1f3
Sawyer Maloney <sawyerdmaloney@gmail.com>
2024-10-21 17:19:23 -0400

    trying to parallelize api calls

--- get_item_data.py
+++ get_item_data.py
@@ -3,18 +3,15 @@ import time
 import json
 import os
 import datetime
+from multiprocessing import Process
 # ------------------------------ TODO ------------------------------ # 
 """
     Use timeseries and loop over ids, might be a speed increase
     Depends on number of ids versus 365 limit
     Possibly only take a subsection of items (~140? Good items?)
+
+    Or multithread using pool to make multiple api requests 
 """
-
-def get_mean(item_id, item):
-    if item["lowPriceVolume"] != None and item["highPriceVolume"] != None and item["avgHighPrice"] != 0 and item["avgLowPrice"] != 0 and item["lowPriceVolume"] != 0 and item["highPriceVolume"] != 0:
-        total_volume = item["lowPriceVolume"] + item["highPriceVolume"]
-        return (item["avgHighPrice"] * item["highPriceVolume"] + item["avgLowPrice"] * item["lowPriceVolume"]) / total_volume
-
 
 url = "https://prices.runescape.wiki/api/v1/osrs/5m?timestamp="
 mapping = "https://prices.runescape.wiki/api/v1/osrs/mapping"
@@ -39,6 +36,18 @@ else:
     with open("item_names.json", "w") as item_names_json:
         json.dump(item_names, item_names_json)
 
+def get_data(thread_num, start, num_iters, interval_time, data):
+    print(f"thread {thread_num} range: from {start - thread_num * num_iters * interval_time} to {start - (thread_num + 1) * num_iters * interval_time}")
+    for i in range(start - thread_num * int(num_iters / 10) * interval_time * 60, start - (thread_num + 1) * int(num_iters / 10) * interval_time * 60):
+        response = requests.get(url + timestamp, headers=headers).json()["data"]
+
+        for item_id in items.keys():
+            if item_id in response.keys():
+                item = response[item_id]
+                data[item_id].insert(0, (item["avgLowPrice"], item["lowPriceVolume"], item["avgHighPrice"], item["highPriceVolume"]))
+
+        if i % 10:
+            print(f"thread {thread_num}: {i}")
 
 # specified_time = time.struct_time((year, month, day, hour, minute, second, 0, 0, 0))
 # print(specified_time)
@@ -68,27 +77,43 @@ start_time = int(time.time())
 while start_time % 300 != 0:
     start_time -= 1
 
-if os.path.exists("items_raw.json"):
-    print("loading from items_raw. Delete items_raw.json if you want to update data.")
-    with open("items_raw.json", "r") as raw:
-        items = json.load(raw)
-else:
-    print("items_raw.json not found. Using API calls.")
-    print(f"number of intervals: {intervals_per_day * number_of_days}")
-    for i in range(int(intervals_per_day * number_of_days)):
-        # specified_time = time.struct_time((year, month, day, hour, minute, second, 0, 0, 0))
-        timestamp = int(start_time) - (5 * 60 * i) # the number of seconds in the number of five minute chunks that we're subtracting
-        timestamp = str(timestamp)
+if os.path.exists("items_raw.json") :
+    check_raw = input("items_raw.json exists. would you like to make API calls? (y/n): ")
+    if check_raw == "n":
+        with open("items_raw.json", "r") as raw:
+            items = json.load(raw)
+    else:
+        print("items_raw.json not found. Using API calls.")
+        print(f"number of intervals: {intervals_per_day * number_of_days}")
 
-        response = requests.get(url + timestamp, headers=headers).json()["data"]
+        # parallelizing by splitting the calls into ten sections
+        parallel = 10
+        data = [[] for _ in range(parallel)] # for holding data
+        processes = []
+        for _ in range(parallel):
+            print(f"starting process {_}")
+            processes.append(Process(target=get_data, args=(_, start_time, parallel, interval_time, data[_])))
+            processes[_].start()
+        for _ in processes:
+            print(f"joining process {_}")
+            _.join()
+    """
+        for i in range(int(intervals_per_day * number_of_days)):
+            timestamp = int(start_time) - (5 * 60 * i) # the number of seconds in the number of five minute chunks that we're subtracting
+            timestamp = str(timestamp)
 
-        for item_id in items.keys():
-            if item_id in response.keys():
-                item = response[item_id]
-                items[item_id].insert(0, (item["avgLowPrice"], item["lowPriceVolume"], item["avgHighPrice"], item["highPriceVolume"]))
+            response = requests.get(url + timestamp, headers=headers).json()["data"]
 
-        if i % 10 == 0:
-            print(i)
+            for item_id in items.keys():
+                if item_id in response.keys():
+                    item = response[item_id]
+                    items[item_id].insert(0, (item["avgLowPrice"], item["lowPriceVolume"], item["avgHighPrice"], item["highPriceVolume"]))
+
+            if i % 10 == 0:
+                print(i)
+    """
+
+
 
 with open("items_raw.json", "w") as raw:
     json.dump(items, raw)
