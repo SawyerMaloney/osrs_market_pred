4e2be6b2576493feead2b48bca1ae3089c9a57f9
Sawyer Maloney <sawyerdmaloney@gmail.com>
2024-10-21 14:20:32 -0400

    targetting just one item (cannonballs)

--- market_predictor.py
+++ market_predictor.py
@@ -5,6 +5,7 @@
     - I think having smaller targets (like all the items will be ~ 200-1000 gp) will help with this
     - so that the model does not have to learn long enough to push the weights up that high
     - possibly also remove the volumes? Just predict the high/low prices?
+- possibly not needed to normalize the price data, just have better & more specific targets
 
 """
 
@@ -67,12 +68,18 @@ data_dtype = torch.float
 
 timeseries_total_length = len(all_data[item_ids[0]])
 number_of_items = len(item_ids)
-fields_per_item = 4 # avg high/low, vol high/low --> four total fields
+# fields_per_item = 4 # avg high/low, vol high/low --> four total fields
 
-data = torch.zeros((timeseries_total_length, number_of_items, fields_per_item), dtype=data_dtype)
+# changing data manipulation to be 1d--just prices
+for key in all_data.keys():
+    for item in range(len(all_data[key])):
+        all_data[key][item] = all_data[key][item][0]
+
+# data = torch.zeros((timeseries_total_length, number_of_items, fields_per_item), dtype=data_dtype)
+data = torch.zeros((timeseries_total_length, number_of_items), dtype=data_dtype)
 # copy data over
 for i in range(len(item_ids)):
-    data[:,i,:] = torch.tensor(all_data[item_ids[i]], dtype=data_dtype)
+    data[:,i] = torch.tensor(all_data[item_ids[i]], dtype=data_dtype)
 
 # ----------------- model definition ----------------- #
 
@@ -92,7 +99,6 @@ class PricePredictorRNN(nn.Module):
         out = self.fc(rnn_out[-1, :])  # Use the last time step output
         return out
 
-
 def train_one_epoch():
     min_loss = 100000000000
     for i in range(epoch_length):
@@ -101,7 +107,7 @@ def train_one_epoch():
         index = torch.randint(0, len(all_data[item_ids[0]]) - sequence_length - 1, (1,))
         # time series
         # inputs = data[:, index:index + sequence_length]
-        inputs = data[index:index + sequence_length, :]
+        inputs = data[index:index + sequence_length]
 
 
         # normalize the inputs -- hopefully speed up training and make more accurate
@@ -110,7 +116,8 @@ def train_one_epoch():
         # inputs = F.normalize(inputs, dim=0)
 
         # the target value (five minutes in the future)
-        labels = torch.squeeze(data[index + sequence_length + 1, :])
+        # taking only id=2 
+        labels = torch.squeeze(data[index + sequence_length + 1, item_ids.index("2")])
 
         optimizer.zero_grad()
         
@@ -139,13 +146,15 @@ def train_one_epoch():
 sequence_length = 64
 epoch_length = 1000
 
-input_size = fields_per_item
-hidden_size = 64
-model = PricePredictorRNN(input_size, hidden_size, input_size, num_layers = 1)
+# input_size = (number_of_items, fields_per_item)
+input_size = number_of_items
+hidden_size = 8
+output_size = 1
+model = PricePredictorRNN(input_size, hidden_size, output_size, num_layers = 1)
 
 criterion = nn.MSELoss()
 
-learning_rate = 0.001
+learning_rate = 0.01
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 train_one_epoch()
